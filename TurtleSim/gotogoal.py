5ef0fb59b81f062e3fcf4d9e4cc3a6ef1d511cdd
Gaurav Sangwan <138971930+sangwan7gaurav@users.noreply.github.com>
2024-07-06 11:32:14 +0530

    Update gotogoal.py

--- TurtleSim/gotogoal.py
+++ TurtleSim/gotogoal.py
@@ -4,8 +4,6 @@ import rospy
 from geometry_msgs.msg import Twist
 from turtlesim.msg import Pose
 import math
-
-# Global variable for the turtle's pose
 pose = Pose()
 
 def pose_callback(data):
@@ -13,29 +11,19 @@ def pose_callback(data):
     pose = data
 
 def go_to_goal():
-    # Initialize the ROS node
     rospy.init_node('go_to_goal_simple', anonymous=True)
-
-    # Create a publisher to send velocity commands
     velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
-
-    # Create a subscriber to get the turtle's pose
     rospy.Subscriber('/turtle1/pose', Pose, pose_callback)
 
     rate = rospy.Rate(10)  # 10 Hz
-
-    # Set the goal position
     goal_x = 5.5
     goal_y = 5.5
 
     while not rospy.is_shutdown():
-        # Calculate the distance to the goal
         distance = math.sqrt((goal_x - pose.x)**2 + (goal_y - pose.y)**2)
 
-        # Create a Twist message
         vel_msg = Twist()
 
-        # Move towards the goal
         if distance > 0.1:
             vel_msg.linear.x = 1.5
             vel_msg.angular.z = 4 * (math.atan2(goal_y - pose.y, goal_x - pose.x) - pose.theta)
@@ -43,10 +31,8 @@ def go_to_goal():
             vel_msg.linear.x = 0
             vel_msg.angular.z = 0
 
-        # Publish the velocity command
         velocity_publisher.publish(vel_msg)
 
-        # Sleep to maintain the loop rate
         rate.sleep()
 
 if __name__ == '__main__':
